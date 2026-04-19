e948a69bf0e75ae8ccff69c4676c0088d3203305
Jaswant Vathanam <BJaswant2005@outlook.com>
2026-04-20 01:57:08 +0530

    Add spotify-common.js with utility functions

---
+++ scripts/spotify-common.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+
+async function getSpotifyAccessToken() {
+  const body = new URLSearchParams();
+  body.append('grant_type', 'refresh_token');
+  body.append('refresh_token', process.env.SPOTIFY_REFRESH_TOKEN);
+  body.append('client_id', process.env.SPOTIFY_CLIENT_ID);
+  body.append('client_secret', process.env.SPOTIFY_CLIENT_SECRET);
+
+  const res = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body
+  });
+
+  const json = await res.json();
+  return json.access_token;
+}
+
+function writeSvg(path, svg) {
+  fs.writeFileSync(`tiles/${path}`, svg, 'utf8');
+}
+
+function escape(str) {
+  return (str || '').replace(/&/g, '&amp;').replace(/</g, '&lt;');
+}
+
+module.exports = { getSpotifyAccessToken, writeSvg, escape };
